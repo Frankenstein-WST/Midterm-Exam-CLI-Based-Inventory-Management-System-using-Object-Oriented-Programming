7266e92aea40f3aacc9e9d75393eeec634d57f4a
Frankenstein-WST <fbm2023-8532-96773@bicol-u.edu.ph>
2026-04-10 19:37:35 +0800

    feat: add Category model

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+namespace InventorySystem.Models
+{
+    public class Category
+    {
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Category(int id, string name, string description = "")
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+    }
+}
